41cfe8077931f28f8ebcd58295fff72c51fe735b
Haisom khan <devil.gamer.maestro@gmail.com>
2025-09-06 11:05:45 +0000

    remove this  (_for element nav_)

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -43,14 +43,7 @@ export default function SettingsPage() {
       <div className="mx-auto grid w-full max-w-6xl gap-2">
         <h1 className="text-3xl font-semibold">Settings</h1>
       </div>
-      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav className="grid gap-4 text-sm text-muted-foreground">
-          <a href="#" className="font-semibold text-primary">
-            General
-          </a>
-          <a href="#">Appearance</a>
-          <a href="#">Notifications</a>
-        </nav>
+      <div className="mx-auto grid w-full max-w-6xl items-start gap-6">
         <div className="grid gap-6">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
