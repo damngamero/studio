5471855ebbee5e3f99985fa5ee3402f06dbc12f2
Haisom khan <devil.gamer.maestro@gmail.com>
2025-09-07 10:29:22 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,7 +4,7 @@
 
 import Link from "next/link";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@rodix-ui/react-hook-form-resolvers";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { AppLayout } from "@/components/AppLayout";
 import { Button } from "@/components/ui/button";
