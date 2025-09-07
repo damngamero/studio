2b82836718dd443894b173b2ca8b0285ced3d4ce
Haisom khan <devil.gamer.maestro@gmail.com>
2025-09-07 14:19:32 +0000

    https://pixabay.com/sound-effects/sharp-pop-328170/

--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -1,6 +1,6 @@
 
 export const soundUrls = {
-  click: 'https://cdn.pixabay.com/audio/2021/08/04/audio_bb630cc098.mp3', // Switch click
+  click: 'https://cdn.pixabay.com/audio/2024/04/10/audio_5145c1a53b.mp3', // Sharp Pop
   notification: 'https://cdn.pixabay.com/audio/2022/03/10/audio_e09618c9a8.mp3', // Notification bell
   achievement: 'https://cdn.pixabay.com/audio/2022/03/24/audio_a752a30999.mp3', // Success fanfare
 };
