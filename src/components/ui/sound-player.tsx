638eb7f63c40c6deb33b12642bb4b1a0b97aae2a
Haisom khan <devil.gamer.maestro@gmail.com>
2025-09-07 14:28:01 +0000

    use the one i gave u'

--- src/components/ui/sound-player.tsx
+++ src/components/ui/sound-player.tsx
@@ -6,9 +6,9 @@
 export function SoundPlayer() {
   return (
     <>
-      <audio id="audio-click" src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/bell.png" preload="auto"></audio>
+      <audio id="audio-click" src="https://cdn.pixabay.com/download/audio/2023/09/24/audio_34b8c6a0b2.mp3" preload="auto"></audio>
       <audio id="audio-notification" src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/bell.png" preload="auto"></audio>
-      <audio id="audio-achievement" src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/bell.png" preload="auto"></audio>
+      <audio id="audio-achievement" src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/tada.png" preload="auto"></audio>
     </>
   );
 }
