73b966a2f447961136837540a323ed040ce2835f
Haisom khan <devil.gamer.maestro@gmail.com>
2025-09-07 14:20:07 +0000

    nope it doesnt play

--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -1,6 +1,6 @@
 
 export const soundUrls = {
-  click: 'https://cdn.pixabay.com/audio/2024/04/10/audio_5145c1a53b.mp3', // Sharp Pop
+  click: 'https://cdn.pixabay.com/download/audio/2023/09/14/audio_37632ff397.mp3', // Sharp Pop
   notification: 'https://cdn.pixabay.com/audio/2022/03/10/audio_e09618c9a8.mp3', // Notification bell
   achievement: 'https://cdn.pixabay.com/audio/2022/03/24/audio_a752a30999.mp3', // Success fanfare
 };
